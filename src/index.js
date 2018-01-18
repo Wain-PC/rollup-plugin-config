a94ab68599ed103576d225477d72ea3105f1f2e1
Pavel Dranichnikov <Pavel.Dranichnikov@kaspersky.com>
2018-01-18 14:24:37 +0300

    -Refactored the replacement algorithm

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import * as babylon from 'babylon';
 
 export default function replaceConfig(config = {}) {
 	const keys = Object.keys(config),
-		replacer = (configObj, propertiesPathArray, rootKey) => {
-			let configValue = configObj[rootKey];
+		replacer = (configObj, propertiesPathArray) => {
+			let configValue = configObj;
 			do {
-				const key = propertiesPathArray.pop();
+				const key = propertiesPathArray.shift();
 				configValue = configValue[key];
 			} while (configValue !== undefined && propertiesPathArray.length); // eslint-disable-line
 			//Replace only when the config actually has the value required
@@ -15,33 +15,23 @@ export default function replaceConfig(config = {}) {
 			}
 			return null;
 		},
-		plugin = function () {
-			let pathArr, level = 0, isReplaceable;
+		plugin = function ({types: t}) {
 			return {
 				visitor: {
-					MemberExpression: {
-						enter(path) {
-							//Reset the `replaceable` flag if we're beginning to explore the MemberExpression
-							if (!level) {
-								isReplaceable = false;
-								pathArr = [];
+					MemberExpression(path) {
+						//If the expression contains the root key of the config, we should travel atop of it
+						// to find a full path and replace it with the config value
+						if (path.node.object.name && ~keys.indexOf(path.node.object.name)) {
+							let currentPath = path, prevPath, pathArr = [];
+							pathArr.push(path.node.object.name);
+							while (t.isMemberExpression(currentPath.node)) {
+								prevPath = currentPath;
+								pathArr.push(currentPath.node.property.name);
+								currentPath = currentPath.parentPath;
 							}
-							level += 1;
-							pathArr.push(path.node.property.name);
-							// If the node has its object's name (final node), we're ready to make an assumption
-							// about its replaceability
-							if (~keys.indexOf(path.node.object.name)) {
-								isReplaceable = path.node.object.name;
-							}
-						},
-						exit(path) {
-							level -= 1;
-							//Full path is now constructed, let's find a corresponding config value
-							if (!level && isReplaceable) {
-								const value = replacer(config, pathArr, isReplaceable);
-								const ast = babylon.parse(`var code = ${JSON.stringify(value)};`);
-								path.replaceWith(ast.program.body[0].declarations[0].init);
-							}
+							const value = replacer(config, pathArr);
+							const ast = babylon.parse(("var code = " + (JSON.stringify(value)) + ";"));
+							prevPath.replaceWith(ast.program.body[0].declarations[0].init);
 						}
 					}
 				}
@@ -51,7 +41,7 @@ export default function replaceConfig(config = {}) {
 	return {
 		name: 'config',
 		transform(bundleCode) {
-			const {code, map} = babel.transform(bundleCode, { plugins: [plugin], sourceMaps: true });
+			const {code, map} = babel.transform(bundleCode, {plugins: [plugin], sourceMaps: true});
 			return {code, map};
 		}
 	};
